0511ce04af326f32e9c98f52aab3009022597732
delso-ferreira <delsofelipe@gmail.com>
2023-05-21 14:13:16 -0300

    att lint

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,6 @@
 // Coloque aqui suas actions
-import { DELETE_TABLE_DATA, USER_SUBMIT, WALLET_REQUEST, WALLET_SUBMIT } from './actionTypes';
+import { DELETE_TABLE_DATA, USER_SUBMIT, WALLET_REQUEST,
+  WALLET_SUBMIT } from './actionTypes';
 
 const submitUserForm = (userProfile) => ({
   type: USER_SUBMIT,
